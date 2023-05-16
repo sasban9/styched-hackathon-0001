51ecb58059d9fffa2b88b7839263092dab12e2cc
Harshit <harshitkapoor.kapoor94@gmail.com>
2023-05-17 01:56:50 +0530

    Added button to delete paticular tailor

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,19 @@ const {Order, Sku, Tailor} = require('./module');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 uri = 'mongodb+srv://admin:admin@styched.63sksdt.mongodb.net/styched?retryWrites=true&w=majority'
 
 app.get('/getTailors', async (req, res) => {
     const tailors = await Tailor.find({});
     res.send(tailors);
+})
+
+app.post('/deleteTailor', async (req, res) => {
+    Tailor.deleteOne({username: req.body.username})
+    .then(() => res.send({code: 2, msg: 'Tailor deleted successfully'}))
+    .catch(() => res.send({code: 1, msg: 'Something wents Wrong'}))
 })
 
 app.listen(8000, () => {
--- frontend/src/components/Tailor.js
+++ frontend/src/components/Tailor.js
@@ -6,12 +6,6 @@ function Tailor() {
 
     const [tailors, setTailors] = useState([])
 
-    const goToTailor = () => {
-    }
-
-    const deteteTailor = () => {
-    }
-
     useEffect(() => {
         axios.get('http://localhost:8000/getTailors')
             .then(res => {
@@ -20,19 +14,33 @@ function Tailor() {
             })
     }, [])
 
+    const goToTailor = () => {
+    }
+
+    const deteteTailor = (username) => {
+        axios.post('http://localhost:8000/deleteTailor', { username: username })
+            .then(res => {
+                if (res.data.code === 1) {
+                    window.alert(res.data.msg)
+                } else {
+                    window.location.reload()
+                }
+            })
+    }
+
     return (
         <div className='Tailors'>
             <div className='tailors-title'>Select Tailor Below</div>
             <div className='tailor'>
-            {tailors.map((tailor) => {
-                return (
-                    <div key={tailor.username} className='tailor-info' onClick={() => goToTailor(tailor.username)}>
-                        <div className='tailor-name'>Name: {tailor.name}</div>
-                        <div className='tailor-username'>Username: {tailor.username}</div>
-                        <button className='tailor-delete' onClick={() => deteteTailor(tailor.username)}>Delete</button>
-                    </div>
-                )
-            })}
+                {tailors.map((tailor) => {
+                    return (
+                        <div key={tailor.username} className='tailor-info' onClick={() => goToTailor(tailor.username)}>
+                            <div className='tailor-name'>Name: {tailor.name}</div>
+                            <div className='tailor-username'>Username: {tailor.username}</div>
+                            <button className='tailor-delete' onClick={() => deteteTailor(tailor.username)}>Delete</button>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
