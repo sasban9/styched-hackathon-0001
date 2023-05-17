216f62956d6aaa832a419e430ae9a01c8506e379
Saswata Banerjee <azure.saswata@gmail.com>
2023-05-18 02:28:27 +0530

    UI changes to components
    can the tailors swipe on SKU cards like tinder?

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -31,48 +31,62 @@ function Info() {
     return (
         <div>
             <div className='Info-title'>Details of tailor {tailor.name}</div>
-            <div className='Info-payment'>Your earn ₹{parseInt(tailor.payment / 6)} so far </div>
+            <div className='Info-payment'>Your earn ₹{Math.floor(tailor.payment / 30)*5} so far </div>
             <div className='Info-order'>
                 <div className='Info-process-orders'>
                     <div className='Info-process-title'>Process Order</div>
+                    <div className='Info-complete-details'>
+                        <h4>Order#</h4>
+                        <h4>SKU & Size</h4>
+                        <h4>Commission</h4>
+                    </div>
                     {tailor.processOrders?.map((processOrder, index) => {
                         return (
                             <div className='Info-process-details'>
-                                <div className='Info-process-index'>Order No: {index + 1}</div>
+                                <div className='Info-process-index'><b>#{processOrder._id.substring(16).toUpperCase()}</b><br/>{processOrder.createdAt}<br/>
+                                <button className='Info-markAsComplete-button' onClick={() => markAsCompleteHandler(processOrder)}>Mark as Complete</button>
+                                </div>
                                 <div className='Info-process-order'>
                                     {processOrder.sku?.map((skuUnit) => {
                                         return (
                                             <div className='Info-process-detail'>
-                                                <div>Design Name: {skuUnit.name}</div>
+                                                <div>{skuUnit.name}</div>
                                                 <div>Size: {skuUnit.size}</div>
                                             </div>
                                         )
                                     })}
-                                    <button className='Info-markAsComplete-button' onClick={() => markAsCompleteHandler(processOrder)}>Mark as Complete</button>
                                 </div>
+                                <div>INR {Math.floor(processOrder.price/30)*5}</div>
+
                             </div>
                         )
                     })}
                 </div>
                 <div className='Info-complete-orders'>
-                    <div className='Info-complete-title'>Completed Order</div>
+                    <div className='Info-complete-title' style={{background:'black', color:'white',padding:10}}>Completed</div>
+                    <div style={{padding:10}}><div className='Info-complete-details'>
+                        <h4>Order#</h4>
+                        <h4>SKU & Size</h4>
+                        <h4>Commission</h4>
+                    </div>
                     {tailor.completeOrders?.map((completeOrder, index) => {
                         return (
                             <div className='Info-complete-details'>
-                                <div className='Info-complete-index'>Order No: {index + 1}</div>
+                                <div className='Info-complete-index'><b>#{completeOrder._id.substring(16).toUpperCase()}</b><br/>{completeOrder.createdAt}<br/></div>
                                 <div className='Info-complete-order'>
                                     {completeOrder.sku?.map((skuUnit) => {
                                         return (
                                             <div className='Info-complete-detail'>
-                                                <div>Design Name: {skuUnit.name}</div>
+                                                <div>{skuUnit.name}</div>
                                                 <div>Size: {skuUnit.size}</div>
                                             </div>
                                         )
                                     })}
                                 </div>
+                                <div>INR {Math.floor(completeOrder.price/30)*5}</div>
                             </div>
                         )
-                    })}
+                    })}</div>
                 </div>
             </div>
         </div>
